23c7e14d6061c55bc7920818e5e1153630a5d947
shahadathshahriarakash <shahadathshahriarakash@gmail.com>
2022-04-18 17:12:13 +0600

    Init

---
+++ Operators/Operators.js
@@ -0,0 +1,46 @@
+const x = 55;
+const y = 35;
+const z = 55;
+
+// Arithmetic Operators
+
+x + y; // Addition
+x - y; // Subtraction
+x * y; // Multiplication
+x / y; // Division
+x % y; // Modulus
+x ** y; // Exponentiation
+
+// Assignment Operators
+
+x += 10; // Addition Assignment
+x -= 10; // Subtraction Assignment
+x *= 10; // Multiplication Assignment
+x /= 10; // Division Assignment
+x %= 10; // Modulus Assignment
+x **= 10; // Exponentiation Assignment
+
+// Comparison Operators
+
+x > y; // Greater Than
+x < y; // Less Than
+x >= y; // Greater or Equal to
+x <= y; // Less or Equal to
+x == y; // Lose Equal to
+x != y; // Lose Not Equal to
+x === y; // Strict Equal to
+x !== y; // Strict Not Equal to
+
+// Ternary Operators
+
+let math = x > y ? 'x is Greater Than y' : 'x is Less or Equal to y';
+
+// Logical Operators
+
+a = x > y;
+b = x < y;
+c = x === z;
+
+a && c // AND (Both Condition True = True)
+a || b // OR (One Condition True = True)
+!c // NOT (Reverse the boolean Value)
